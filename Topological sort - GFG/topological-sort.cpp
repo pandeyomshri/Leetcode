e3eea00da0de8bd09871c7567fd6b39a0d517bc4
Shri om Pandey <115021156+pandeyomshri@users.noreply.github.com>
2023-07-04 17:46:10 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,33 +6,68 @@ using namespace std;
 class Solution
 {
 	public:
-	void dfs(int node, vector<int> adj[], vector<int> &vis, stack<int> &st){
-	    vis[node]=1;
-	    for(auto it: adj[node]){
-	        if(!vis[it]){
-	            dfs(it, adj, vis, st);
-	        }
+// 	void dfs(int node, vector<int> adj[], vector<int> &vis, stack<int> &st){
+// 	    vis[node]=1;
+// 	    for(auto it: adj[node]){
+// 	        if(!vis[it]){
+// 	            dfs(it, adj, vis, st);
+// 	        }
+// 	    }
+// 	     st.push(node);
+// 	}
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<int> vis(V,0);
+// 	    stack<int> st;
+	    
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i])
+// 	        dfs(i, adj, vis, st);
+// 	    }
+	    
+// 	    vector<int> ans;
+// 	    while(!st.empty()){
+// 	        ans.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	    return ans;
+// 	}
+
+vector<int> topoSort(int V, vector<int> adj[]){ //Kahn's algorithm (BFS)R
+    
+    vector<int> indegree(V, 0);
+	
+	for(int i=0;i<V;i++){
+	    for(auto it: adj[i]){
+	        indegree[it]++;
 	    }
-	     st.push(node);
 	}
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    vector<int> vis(V,0);
-	    stack<int> st;
+	
+	queue<int> q;
+	vector<int> ans;
+	
+	for(int i=0;i<V;i++){
+	    if(indegree[i]==0)
+	    q.push(i);
+	}
+	
+	while(!q.empty()){
+	    int node = q.front();
+	    q.pop();
+	    ans.push_back(node);
 	    
-	    for(int i=0;i<V;i++){
-	        if(!vis[i])
-	        dfs(i, adj, vis, st);
+	    for(auto it: adj[node]){
+	        indegree[it]--;
+	        if(indegree[it]==0)
+	          q.push(it);
 	    }
 	    
-	    vector<int> ans;
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
+	}
+	return ans;
+	
+	
 	}
 };
 
